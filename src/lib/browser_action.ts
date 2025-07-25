42d7d895a189c9bfea53ffcc0167d06f822b72b5
ojer <cacaojer@outlook.com>
2025-07-26 00:30:30 +0800

    style: update browser action text color

--- src/lib/browser_action.ts
+++ src/lib/browser_action.ts
@@ -4,28 +4,62 @@ function addModelchangeListener() {
             message.type === "TRIDACTYL_MODE_INDICATOR_CHANGE" &&
             sender?.tab?.id
         ) {
-            const { indicatorText } = message
-            const badgeText =
-                indicatorText.length > 3
-                    ? indicatorText.slice(0, 3)
-                    : indicatorText
             const tabId = sender.tab.id
+            const { indicatorText } = message
+            let badgeText = ""
+            let backgroundColor = "green"
+            switch (indicatorText) {
+                case "insert":
+                    badgeText = "I"
+                    break
+                case "hint":
+                    badgeText = "H"
+                    break
+                case "gobble":
+                    badgeText = "G"
+                    break
+                case "input":
+                    badgeText = "I"
+                    backgroundColor = "blue"
+                    break
+                case "visual":
+                    badgeText = "V"
+                    break
+                case "nmode":
+                    badgeText = "N"
+                    break
+                case "ignore":
+                    badgeText = "I"
+                    backgroundColor = "red"
+                    break
+                case "normal":
+                    badgeText = ""
+                    break
+                default:
+                    badgeText = ""
+            }
+            if (badgeText) {
+                browser.browserAction.setBadgeText({
+                    text: `${badgeText}`,
+                    tabId,
+                })
+                browser.browserAction.setBadgeTextColor({
+                    color: "#FFF",
+                    tabId,
+                })
+                browser.browserAction.setBadgeBackgroundColor({
+                    // color: "#389749",
+                    color: backgroundColor,
+                    tabId,
+                })
+            } else {
+                browser.browserAction.setBadgeText({
+                    text: "",
+                    tabId,
+                })
+            }
             browser.browserAction.setTitle({
                 title: indicatorText,
-                tabId,
-            })
-
-            browser.browserAction.setBadgeText({
-                text: ` ${badgeText[0].toUpperCase()} `,
-                tabId,
-            })
-            browser.browserAction.setBadgeTextColor({
-                color: "#FFF",
-                tabId,
-            })
-            browser.browserAction.setBadgeBackgroundColor({
-                // color: "#389749",
-                color: "#1f9947",
                 tabId,
             })
             return Promise.resolve(1)
